2a43999e5fe0203610a5b490d1b25e2b2729b933
enesbatiny <enesbatin97@hotmail.com>
2020-12-16 23:10:22 +0300

    Personel tables listed

--- src/Personel.java
+++ src/Personel.java
@@ -2,12 +2,14 @@
 public class Personel {
     private int id;
     private String personel_adi;
+    private String personel_soyadi;
     private String personel_password;
     private boolean personel_yetki;
 
-    public Personel(int id, String personel_adi, String personel_password, boolean personel_yetki) {
+    public Personel(int id, String personel_adi, String personel_soyadi, String personel_password, boolean personel_yetki) {
         this.id = id;
         this.personel_adi = personel_adi;
+        this.personel_soyadi = personel_soyadi;
         this.personel_password = personel_password;
         this.personel_yetki = personel_yetki;
     }
@@ -26,6 +28,14 @@ public class Personel {
 
     public void setPersonel_adi(String personel_adi) {
         this.personel_adi = personel_adi;
+    }   
+
+    public String getPersonel_soyadi() {
+        return personel_soyadi;
+    }
+
+    public void setPersonel_soyadi(String personel_soyadi) {
+        this.personel_soyadi = personel_soyadi;
     }
 
     public String getPersonel_password() {
--- src/PersonelIslemleri.java
+++ src/PersonelIslemleri.java
@@ -1,20 +1,76 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Taha
- */
+import java.util.ArrayList;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
+
 public class PersonelIslemleri extends javax.swing.JFrame {
 
     /**
      * Creates new form PersonelIslemleri
      */
     public PersonelIslemleri() {
+        
+        DefaultTableModel model;
         initComponents();
+        model=(DefaultTableModel)PersonelTablo.getModel();
+        try {
+            ArrayList<Personel> personels = getPersonel();
+            for(Personel personel : personels ){
+            Object[] row = {personel.getId(), personel.getPersonel_adi(),personel.getPersonel_soyadi(),personel.getPersonel_password(),personel.isPersonel_yetki()};
+            model.addRow(row);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(PersonelIslemleri.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public ArrayList<Personel> getPersonel() throws SQLException {
+        Connection connection = null;
+        DbHelper DbHelper = new DbHelper();
+        Statement statement = null;
+        //ResultSet ResultSet = new ResultSet();
+
+        ArrayList<Personel> personels = null;
+
+        /*try {
+            //ResultSet resultSet=statement.executeQuery(selectQuery);
+            connection = DbHelper.getConnection();
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from personel");
+            personels = new ArrayList<Personel>();
+            while(resultSet.next()){
+            personels.add(new Personel
+                            (resultSet.getInt("Personel No"),
+                            resultSet.getString("personel_adi"),
+                            resultSet.getString("personel_soyadi"),
+                            resultSet.getString("personel_yetki")
+         */
+        try {
+            connection = DbHelper.getConnection();
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from personel");
+            personels = new ArrayList<Personel>();
+            while (resultSet.next()) {
+                personels.add(new Personel(
+                        resultSet.getInt("id"),
+                        resultSet.getString("personel_adi"),
+                        resultSet.getString("personel_soyadi"),
+                        resultSet.getString("personel_password"),
+                        resultSet.getBoolean("personel_yetki")
+                ));
+
+            }
+
+        } catch (SQLException exception) {
+            DbHelper.showErrorMessage(exception);
+        } finally {
+            statement.close();
+            connection.close();
+        }
+
+        return personels;
     }
 
     /**
@@ -29,6 +85,15 @@ public class PersonelIslemleri extends javax.swing.JFrame {
         popupMenu1 = new java.awt.PopupMenu();
         popupMenu2 = new java.awt.PopupMenu();
         jPopupMenu1 = new javax.swing.JPopupMenu();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        PersonelTablo = new javax.swing.JTable();
+        jTextField1 = new javax.swing.JTextField();
+        jTextField2 = new javax.swing.JTextField();
+        jTextField3 = new javax.swing.JTextField();
+        jTextField4 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
 
         popupMenu1.setLabel("popupMenu1");
 
@@ -36,19 +101,104 @@ public class PersonelIslemleri extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        PersonelTablo.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Personel No", "Personel Adı", " Personel Soyadı", "Personel Şifre", "Personel Yetki"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Boolean.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(PersonelTablo);
+
+        jTextField1.setText("jTextField1");
+
+        jTextField2.setText("jTextField2");
+
+        jTextField3.setText("jTextField3");
+
+        jTextField4.setText("jTextField4");
+        jTextField4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField4ActionPerformed(evt);
+            }
+        });
+
+        jButton1.setText("Personel Ekle");
+
+        jButton2.setText("Personel Güncelle");
+
+        jButton3.setText("Personel Sil");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 651, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(27, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jTextField2, javax.swing.GroupLayout.DEFAULT_SIZE, 150, Short.MAX_VALUE)
+                            .addComponent(jTextField3)
+                            .addComponent(jTextField4)
+                            .addComponent(jTextField1))
+                        .addGap(48, 48, 48)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, 132, Short.MAX_VALUE))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 704, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(27, 27, 27))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 424, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(33, 33, 33)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 51, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(42, 42, 42))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(41, 41, 41))))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jTextField4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField4ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField4ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -86,8 +236,23 @@ public class PersonelIslemleri extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTable PersonelTablo;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JPopupMenu jPopupMenu1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextField4;
     private java.awt.PopupMenu popupMenu1;
     private java.awt.PopupMenu popupMenu2;
     // End of variables declaration//GEN-END:variables
+
+    private static class SQLExeptionstatic {
+
+        public SQLExeptionstatic() {
+        }
+    }
 }
