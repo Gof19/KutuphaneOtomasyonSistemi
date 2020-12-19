d7810ccb3ef6f794390ec908d7276293068ec9fc
Taha Malaldır <tevfikhanmalaldir@gmail.com>
2020-12-19 05:53:35 +0300

    Uye package CRUD updated

--- src/Uye/UyeIslemler.java
+++ src/Uye/UyeIslemler.java
@@ -4,23 +4,22 @@ import KutuphaneOtomasyon.DbHelper;
 import java.sql.*;
 import java.util.ArrayList;
 
-public class UyeIslemleri implements IUyeIslemleri {
+public class UyeIslemler implements IUyeIslemleri {
 
     @Override
     public void Ekle(Uye uye) {
-
         Connection connection = null;
         DbHelper DbHelper = new DbHelper();
         PreparedStatement statement = null;
         try {
             connection = DbHelper.getConnection();
-            String sql = "insert into uye (uye_adi,uye_soyadi,uye_tel,uye_mail,uye_okitap) values(?,?,?,?,?)";
+            String sql = "insert into uye (uye_ad,uye_soyad,uye_tel,uye_mail,uye_okitap) values(?,?,?,?,?)";
             statement = connection.prepareStatement(sql);
-            statement.setString  (1, uye.getAd());
-            statement.setString  (2, uye.getSoyad());
-            statement.setInt    (3, uye.getTel());
-            statement.setString (4, uye.getMail());
-            statement.setInt    (5,uye.getOkitap());
+            statement.setString(1, uye.getAd());
+            statement.setString(2, uye.getSoyad());
+            statement.setInt(3, uye.getTel());
+            statement.setString(4, uye.getMail());
+            statement.setInt(5, uye.getOkitap());
 
             int result = statement.executeUpdate();
             //refreshTable();
@@ -45,7 +44,7 @@ public class UyeIslemleri implements IUyeIslemleri {
         PreparedStatement statement = null;
         try {
             connection = DbHelper.getConnection();
-            String sql = "DELETE FROM uye WHERE id = " + id;
+            String sql = "DELETE FROM uye WHERE uye_id = " + id;
             statement = connection.prepareStatement(sql);
             int result = statement.executeUpdate();
         } catch (SQLException exception) {
@@ -62,21 +61,21 @@ public class UyeIslemleri implements IUyeIslemleri {
 
     @Override
     public void Güncelle(Uye uye, int id) {
-          Connection connection = null;
+        Connection connection = null;
         DbHelper DbHelper = new DbHelper();
         PreparedStatement statement = null;
         //uye Güncelle
         try {
-            String url = "jdbc:mysql://localhost:5555/KutuphaneOtomasyon?useSSL=false&serverTimezone=UTC";
+            //String url = "jdbc:mysql://localhost:5555/KutuphaneOtomasyon?useSSL=false&serverTimezone=UTC";
             connection = DbHelper.getConnection();
 
-            String query = "UPDATE uye SET uye_adi=?, uye_soyadi=?, uye_tel=?, uye_mail=?, uye_okitap=? where id=" + id;
+            String query = "UPDATE uye SET uye_ad=?, uye_soyad=?, uye_tel=?, uye_mail=?, uye_okitap=? where uye_id=" + id;
             PreparedStatement pst = connection.prepareStatement(query);
             pst.setString(1, uye.getAd());
             pst.setString(2, uye.getSoyad());
             pst.setInt(3, uye.getTel());
             pst.setString(4, uye.getMail());
-            pst.setInt(5,uye.getOkitap());
+            pst.setInt(5, uye.getOkitap());
             pst.executeUpdate();
 
         } catch (Exception e) {
@@ -85,21 +84,22 @@ public class UyeIslemleri implements IUyeIslemleri {
 
     @Override
     public ArrayList<Uye> Ara(String text) {
-       
+
         Connection connection = null;
         DbHelper DbHelper = new DbHelper();
         Statement statement = null;
         ArrayList<Uye> uyes = null;
         try {
             connection = DbHelper.getConnection();
-            String sql = "SELECT * FROM uye WHERE uye_adi LIKE '%" + text + "%' or uye_soyadi LIKE '%" + text + "%'";
+            String sql = "SELECT * FROM uye WHERE uye_ad LIKE '%" + text + "%' or uye_soyad LIKE '%" + text + "%'or uye_tel LIKE '%" + text + "%'or uye_mail LIKE '%" + text + "%'";
             statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(sql);
             uyes = new ArrayList<Uye>();
             while (resultSet.next()) {
                 uyes.add(new Uye(
-                        resultSet.getString("uye_adi"),
-                        resultSet.getString("uye_soyadi"),
+                        resultSet.getInt("uye_id"),
+                        resultSet.getString("uye_ad"),
+                        resultSet.getString("uye_soyad"),
                         resultSet.getInt("uye_tel"),
                         resultSet.getString("uye_mail"),
                         resultSet.getInt("uye_okitap")
@@ -125,8 +125,9 @@ public class UyeIslemleri implements IUyeIslemleri {
             uyes = new ArrayList<Uye>();
             while (resultSet.next()) {
                 uyes.add(new Uye(
-                        resultSet.getString("uye_adi"),
-                        resultSet.getString("uye_soyadi"),
+                        resultSet.getInt("uye_id"),
+                        resultSet.getString("uye_ad"),
+                        resultSet.getString("uye_soyad"),
                         resultSet.getInt("uye_tel"),
                         resultSet.getString("uye_mail"),
                         resultSet.getInt("uye_okitap")
--- src/UyeIslemleriForm.java
+++ src/UyeIslemleriForm.java
@@ -1,39 +1,271 @@
 
+import Uye.*;
+import KutuphaneOtomasyon.DbHelper;
+import java.util.ArrayList;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
 public class UyeIslemleriForm extends javax.swing.JFrame {
 
+    DefaultTableModel model;
+
     public UyeIslemleriForm() {
-        initComponents();
+        try {
+            initComponents();
+            refreshTable(this.getUye());
+        } catch (Exception e) {
+        }
+    }
+
+    public void clearTextBox() {
+        uye_ad.setText("");
+        uye_soyad.setText("");
+        uye_tel.setText("");
+        uye_mail.setText("");
+        uye_okitap.setText("");
+    }
+
+    public void refreshTable(ArrayList<Uye> array) {
+        model = (DefaultTableModel) UyeTablo.getModel();
+        model.setRowCount(0);
+        try {
+            ArrayList<Uye> uyeler = array;
+            for (Uye uye : uyeler) {
+                Object[] row = {uye.getId(), uye.getAd(), uye.getSoyad(), uye.getTel(), uye.getMail(), uye.getOkitap()};
+                model.addRow(row);
+            }
+        } catch (Exception e) {
+        }
+    }
+
+    public ArrayList<Uye> getUye() throws SQLException {
+        UyeIslemler islemler = new UyeIslemler();
+        return islemler.Listele();
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        UyeTablo = new javax.swing.JTable();
+        uye_ad = new javax.swing.JTextField();
+        uye_soyad = new javax.swing.JTextField();
+        uye_tel = new javax.swing.JTextField();
+        uye_mail = new javax.swing.JTextField();
+        uye_okitap = new javax.swing.JTextField();
+        uye_guncelle = new javax.swing.JButton();
+        uye_sil = new javax.swing.JButton();
+        uye_ara = new javax.swing.JButton();
+        uye_aratext = new javax.swing.JTextField();
+        uye_ekle = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setText("Uye İslemleri");
+        UyeTablo.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null}
+            },
+            new String [] {
+                "Uye ID", "Uye Ad", "Uye Soyad", "Uye Tel", "Uye Mail", "Okuduğu Kitap S."
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.Integer.class, java.lang.String.class, java.lang.Integer.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(UyeTablo);
+        if (UyeTablo.getColumnModel().getColumnCount() > 0) {
+            UyeTablo.getColumnModel().getColumn(1).setResizable(false);
+            UyeTablo.getColumnModel().getColumn(5).setResizable(false);
+        }
+
+        uye_ad.setText("uye_ad");
+
+        uye_soyad.setText("uye_soyad");
+
+        uye_tel.setText("uye_tel");
+
+        uye_mail.setText("uye_mail");
+
+        uye_okitap.setText("uye_okitap");
+
+        uye_guncelle.setText("uye_guncelle");
+        uye_guncelle.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                uye_guncelleActionPerformed(evt);
+            }
+        });
+
+        uye_sil.setText("uye_sil");
+        uye_sil.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                uye_silActionPerformed(evt);
+            }
+        });
+
+        uye_ara.setText("uye_ara");
+        uye_ara.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                uye_araActionPerformed(evt);
+            }
+        });
+
+        uye_aratext.setText("uye_aratext");
+
+        uye_ekle.setText("uye_ekle");
+        uye_ekle.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                uye_ekleActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(135, 135, 135)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 128, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(137, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(47, 47, 47)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 752, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(146, 146, 146)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(uye_soyad, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(uye_ad, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(uye_tel, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(uye_mail, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(uye_okitap, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(61, 61, 61)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(uye_sil)
+                                    .addComponent(uye_guncelle))
+                                .addGap(199, 199, 199)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(uye_aratext, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(uye_ara)))
+                            .addComponent(uye_ekle))))
+                .addContainerGap(56, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(106, 106, 106)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(125, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(57, 57, 57)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(uye_ad, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(uye_ekle))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(uye_soyad, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(uye_guncelle))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(uye_tel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(uye_sil))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(uye_mail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(uye_okitap, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(73, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(uye_aratext, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(uye_ara)
+                        .addGap(116, 116, 116))))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void uye_guncelleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_uye_guncelleActionPerformed
+        try {
+            int row = UyeTablo.getSelectedRow();
+            String value = (UyeTablo.getModel().getValueAt(row, 0).toString());
+            String ad = uye_ad.getText();
+            String soyad = uye_soyad.getText();
+            int tel = Integer.parseInt(uye_tel.getText().toString());
+            String mail = uye_mail.getText();
+            int okitap = Integer.parseInt(uye_okitap.getText().toString());
+            UyeIslemler islemler = new UyeIslemler();
+            Uye uye = new Uye(ad, soyad, tel, mail, okitap);
+            islemler.Güncelle(uye, Integer.parseInt(value));
+            DefaultTableModel model = (DefaultTableModel) UyeTablo.getModel();
+            model.setRowCount(0);
+            JOptionPane.showMessageDialog(null, "Güncelleme Başarılı");
+            refreshTable(this.getUye());
+            clearTextBox();
+        } catch (SQLException e) {
+
+        }
+    }//GEN-LAST:event_uye_guncelleActionPerformed
+
+    private void uye_ekleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_uye_ekleActionPerformed
+        try {
+            String ad = uye_ad.getText();
+            String soyad = uye_soyad.getText();
+            int tel = Integer.parseInt(uye_tel.getText().toString());
+            String mail = uye_mail.getText();
+            int okitap = Integer.parseInt(uye_okitap.getText().toString());
+            UyeIslemler islemler = new UyeIslemler();
+            Uye uye = new Uye(ad, soyad, tel, mail, okitap);
+            islemler.Ekle(uye);
+            JOptionPane.showMessageDialog(null, "Ekleme Başarılı");
+            refreshTable(this.getUye());
+            clearTextBox();
+        } catch (Exception e) {
+
+        }
+    }//GEN-LAST:event_uye_ekleActionPerformed
+
+    private void uye_silActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_uye_silActionPerformed
+        try {
+            int row = UyeTablo.getSelectedRow();
+            String value = (UyeTablo.getModel().getValueAt(row, 0).toString());
+            UyeIslemler islemler = new UyeIslemler();
+            islemler.Sil(Integer.parseInt(value));
+            DefaultTableModel model = (DefaultTableModel) UyeTablo.getModel();
+            model.setRowCount(0);
+            JOptionPane.showMessageDialog(null, "Silme Başarılı");
+            refreshTable(this.getUye());
+        } catch (Exception e) {
+        }
+    }//GEN-LAST:event_uye_silActionPerformed
+
+    private void uye_araActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_uye_araActionPerformed
+        try {
+            String text = uye_aratext.getText();
+            UyeIslemler islemler = new UyeIslemler();
+            //JOptionPane.showMessageDialog(null, "Silme Başarılı");
+            refreshTable(islemler.Ara(text));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }//GEN-LAST:event_uye_araActionPerformed
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -68,6 +300,17 @@ public class UyeIslemleriForm extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JTable UyeTablo;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField uye_ad;
+    private javax.swing.JButton uye_ara;
+    private javax.swing.JTextField uye_aratext;
+    private javax.swing.JButton uye_ekle;
+    private javax.swing.JButton uye_guncelle;
+    private javax.swing.JTextField uye_mail;
+    private javax.swing.JTextField uye_okitap;
+    private javax.swing.JButton uye_sil;
+    private javax.swing.JTextField uye_soyad;
+    private javax.swing.JTextField uye_tel;
     // End of variables declaration//GEN-END:variables
 }
